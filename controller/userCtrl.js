fea61829095a7992d96add78cd372bc32d527d07
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-08 18:19:41 +0530

    add getAllUsers

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -32,4 +32,14 @@ const loginUserCtrl = asyncHandler(async(req, res) => {
     }
 });
 
-module.exports = {createUser, loginUserCtrl};
+// Get all users
+const getAllUsers = asyncHandler(async (req, res) => {
+    try {
+        const getUsers = await User.find();
+        res.json(getUsers);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = {createUser, loginUserCtrl, getAllUsers};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createUser, loginUserCtrl } = require('../controller/userCtrl');
+const { createUser, loginUserCtrl, getAllUsers } = require('../controller/userCtrl');
 const router = express.Router();
 
 router.post('/register', createUser);
 router.post('/login', loginUserCtrl);
+router.get('/all-users', getAllUsers);
 
 module.exports = router;
