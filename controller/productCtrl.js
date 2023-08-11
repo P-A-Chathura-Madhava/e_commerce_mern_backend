8f7077ef018a8eae1b9eccd04b5508095696990b
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-11 18:28:34 +0530

    apply addToWishlist functions

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,6 +1,7 @@
 const Product = require('../models/productModel');
 const asyncHandler = require('express-async-handler');
 const slugify = require('slugify');
+const User = require('../models/userModel');
 
 const createProduct = asyncHandler(async (req, res) => {
     try {
@@ -98,4 +99,38 @@ const deleteaProduct = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = {createProduct, getaProduct, getAllProduct, updateProduct, deleteaProduct};
+const addToWishlist = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    const { prodId } = req.body;
+    try {
+      const user = await User.findById(_id);
+      const alreadyadded = user.wishlist.find((id) => id.toString() === prodId);
+      if (alreadyadded) {
+        let user = await User.findByIdAndUpdate(
+          _id,
+          {
+            $pull: { wishlist: prodId },
+          },
+          {
+            new: true,
+          }
+        );
+        res.json(user);
+      } else {
+        let user = await User.findByIdAndUpdate(
+          _id,
+          {
+            $push: { wishlist: prodId },
+          },
+          {
+            new: true,
+          }
+        );
+        res.json(user);
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+});
+
+module.exports = {createProduct, getaProduct, getAllProduct, updateProduct, deleteaProduct, addToWishlist};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createProduct, getaProduct, getAllProduct, updateProduct, deleteaProduct } = require('../controller/productCtrl');
+const { createProduct, getaProduct, getAllProduct, updateProduct, deleteaProduct, addToWishlist } = require('../controller/productCtrl');
 const router = express.Router();
 const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
 
 router.post('/', authMiddleware, isAdmin, createProduct);
 router.get('/:id', getaProduct);
+router.put('/wishlist', authMiddleware, addToWishlist);
 router.put('/:id', authMiddleware, isAdmin, updateProduct);
 router.delete('/:id', authMiddleware, isAdmin, deleteaProduct);
 router.get('/', getAllProduct);
