c0696cf78be35388d45214f36e0f2e73bc05a275
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-11 13:10:45 +0530

    apply updateBlog function

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -6,13 +6,22 @@ const validateMongoDbId = require("../utils/validateMongodbid");
 const createBlog = asyncHandler(async (req, res) => {
     try {
       const newBlog = await Blog.create(req.body);
-      res.json({
-        status: 'success',
-        newBlog,
-      });
+      res.json(newBlog);
     } catch (error) {
       throw new Error(error);
     }
 });
 
-module.exports = {createBlog};
+const updateBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+      const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.json(updateBlog);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+
+module.exports = {createBlog, updateBlog};
