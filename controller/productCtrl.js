e444f5a3d0dde5047eccb3cff4d78e671ef10517
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-11 10:13:01 +0530

    apply sorting

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -44,12 +44,22 @@ const getaProduct = asyncHandler(async (req, res) => {
 
 const getAllProduct = asyncHandler(async (req, res) => {
     try {
+        // Filtering
         const queryObj = {...req.query};
         const excludeFields = ['page', 'sort', 'limit',' fields'];
         excludeFields.forEach(el => delete queryObj[el]);
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
         let query = Product.find(JSON.parse(queryStr));
+
+        // Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
         
         const product = await query;
         res.json(product);
