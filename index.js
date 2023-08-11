6058fdad2d4b52e6eaf0cc895090c5d54628c69e
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-11 17:23:59 +0530

    apply brandCategory functions

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const productRouter = require('./routes/productRoute');
 const blogRouter = require('./routes/blogRoute');
 const categoryRouter = require('./routes/prodcategoryRoute');
 const blogcategoryRouter = require('./routes/blogCatRoute');
+const brandRouter = require('./routes/brandRoute');
 const bodyParser = require('body-parser');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const coockieParser = require('cookie-parser');
@@ -23,6 +24,8 @@ app.use('/api/product', productRouter);
 app.use('/api/blog', blogRouter);
 app.use('/api/category', categoryRouter);
 app.use('/api/blogcategory', blogcategoryRouter);
+app.use('/api/blogcategory', blogcategoryRouter);
+app.use('/api/brand', brandRouter);
 
 app.use(notFound);
 app.use(errorHandler);
