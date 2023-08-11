b6541c9a7542f316fef19049fcde2df27c3360c6
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-11 16:25:29 +0530

    apply updateCategory function

--- controller/prodcategoryCtrl.js
+++ controller/prodcategoryCtrl.js
@@ -9,6 +9,19 @@ const createCategory = asyncHandler(async (req, res) => {
     } catch (error) {
       throw new Error(error);
     }
-  });
+});
 
-module.exports = {createCategory};
+const updateCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {createCategory, updateCategory};
--- routes/prodcategoryRoute.js
+++ routes/prodcategoryRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const {createCategory} = require("../controller/prodcategoryCtrl.js");
+const {createCategory, updateCategory} = require("../controller/prodcategoryCtrl.js");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware.js");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createCategory);
-// router.put("/:id", authMiddleware, isAdmin, updateCategory);
+router.put("/:id", authMiddleware, isAdmin, updateCategory);
 // router.delete("/:id", authMiddleware, isAdmin, deleteCategory);
 // router.get("/:id", getCategory);
 // router.get("/", getallCategory);
